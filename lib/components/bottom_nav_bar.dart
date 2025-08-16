e48b8a8b172cebe14e8e135ac20f792210883990
Abdulmalek ESUGHI <abdul.esughi@gmail.com>
2025-08-16 14:21:03 +0200

    few changes to the bottom bar

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -7,9 +7,14 @@ class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
       child: GNav(
         color: Colors.grey[400],
         activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade200,
+        tabBorderRadius: 25,
+        mainAxisAlignment: MainAxisAlignment.center,
         tabs: const [
           GButton(icon: Icons.home, text: 'Shop'),
           GButton(icon: Icons.shopping_bag_rounded, text: 'Cart'),
